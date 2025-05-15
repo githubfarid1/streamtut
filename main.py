3f3875bed7d7e3c120e931837fe060722da8cbfe
Mohamad Farid Saleh <github.farid1@gmail.com>
2025-05-15 10:28:22 +0900

    update script

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+URI = "mongodb+srv://opencartplugin:Makian1!@mycluster.3eoowbm.mongodb.net/?retryWrites=true&w=majority&appName=mycluster"
+DBNAME = "parkdb"
 
-st.write("hello wai")
-st.write("hello wai")
+client = MongoClient(URI, server_api=ServerApi('1'))
+db = client[DBNAME]
+
+print("Loading User Data ...", flush=True, end="")
+docs = db.userAccess.find()
+ALLOWED_IDS = [doc['userid'] for doc in docs]
+
+
+st.write(ALLOWED_IDS)
+# st.write("hello wai")
 
